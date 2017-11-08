abb883de7d7bd66d725b999e1fb13927992cee09
Helder <helder@yarilabs.com>
2017-11-08 11:23:07 +0000

    initial commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+/* web: elixir -pa _build/prod/consolidated -S mix phoenix.server */
+web: MIX_ENV=prod mix phoenix.server
---
+++ config/prod.exs
@@ -0,0 +1,56 @@
+use Mix.Config
+
+# For production, we configure the host to read the PORT
+# from the system environment. Therefore, you will need
+# to set PORT=80 before running your server.
+#
+# You should also configure the url host to something
+# meaningful, we use this information when generating URLs.
+config :chat, Chat.Endpoint,
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "mercurio-chat.herokuapp.com", port: 443],
+  check_origin: false,
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+  # force_ssl: [rewrite_on: [:x_forwarded_proto]],
+config :chat, Chat.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
+
+# ## SSL Support
+#
+# To get SSL working, you will need to add the `https` key
+# to the previous section:
+#
+#  config:chat, Chat.Endpoint,
+#    ...
+#    https: [port: 443,
+#            keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
+#            certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
+#
+# Where those two env variables point to a file on
+# disk for the key and cert.
+
+# Do not print debug messages in production
+config :logger, level: :info
+
+# ## Using releases
+#
+# If you are doing OTP releases, you need to instruct Phoenix
+# to start the server for all endpoints:
+#
+#     config :phoenix, :serve_endpoints, true
+#
+# Alternatively, you can configure exactly which server to
+# start per endpoint:
+#
+#     config :chat, Chat.Endpoint, server: true
+#
+
+# Finally import the config/prod.secret.exs
+# which should be versioned separately.
+# import_config "prod.secret.exs"
